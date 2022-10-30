cf28eebfcc9035b143637e02cec6149a20f63625
Bruno <br.ananias@gmail.com>
2022-10-30 14:41:06 -0300

    Ajustes para remover métodos não utilizados, após conexão com mongo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 //Fazer uma conexão e se der erro, printar o erro no console
 db.on("error", console.log.bind(console, 'Erro de conexão'))
@@ -14,55 +15,9 @@ const app = express();
 //declarar que o express está usando json. O que chega via json/put, vai ser transformado em objeto.
 app.use(express.json());
 
-/*const livros =[
-    {id: 1, "titulo":"Senhos dos Aneis"},
-    {id: 2, "titulo":"o Hobbit"}
-]*/
-
-app.get('/', (req, res)=>{
-    res.status(200).send('Curso de node')
-});
-//listar todos
-app.get('/livros',(req, res)=>{
-    livros.find((err, livros)=>{
-        res.status(200).json(livros)
-    })
-});
-//listar um id especifico
-app.get('/livros/:id',(req, res)=>{
-    //Criar uma variável index, e passar ela pra nossa fnc busca_livros.
-    //Essa variável vem da URL
-    let index = busca_livro(req.params.id);
-    //Abaixo, vamos procurar ela dentro do array de livros e transformar o titulo, com o que vem da url
-    res.json(livros[index]);
-
-})
-//Cadastrar
-app.post('/livros', (req, res)=>{
-    livros.push(req.body);
-    res.status(201).send('Livro Cadastrado com Sucesso!!')
-})
-//put = atualizar
-app.put('/livros/:id',(req, res)=>{
-    //Criar uma variável index, e passar ela pra nossa fnc busca_livros.
-    //Essa variável vem da URL
-    let index = busca_livro(req.params.id);
-    //Abaixo, vamos procurar ela dentro do array de livros e transformar o titulo, com o que vem da url
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-
-})
-//Deletar
-app.delete('/livros/:id', (req, res)=>{
-    let {id} = req.params;
-    let index = busca_livro(id);
-    livros.splice(index,1)
-    res.send(`Livro ${id} removido.`)
-})
+//passamos o arquivo routes para o nosso app.
+routes(app);
 
 
-function busca_livro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
 
 export default app;
